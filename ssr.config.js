d77f83ba8e6849963bdbf3f5f71e2ea10cfd73e2
huadong zuo <admin@zuohuadong.cn>
2019-11-29 13:03:35 +0800

    Create ssr.config.js

---
+++ ssr.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  template: './index.html',
+  maxAge: 60 * 60 * 1000,
+  config: {
+    basePath: 'https://docs.nestjs.cn/',
+    alias: {
+      '/de-de/changelog': '/changelog',
+      '/zh-cn/changelog': '/changelog',
+      '/changelog': ''
+    },
+    auto2top: true,
+    coverpage: true,
+    executeScript: true,
+    loadSidebar: true,
+    loadNavbar: true,
+    mergeNavbar: true,
+    maxLevel: 4,
+    subMaxLevel: 2,
+    name: 'docsify',
+    search: {
+      noData: {
+        '/de-de/': 'Keine Ergebnisse!',
+        '/zh-cn/': '没有结果!',
+        '/': 'No results!'
+      },
+      paths: 'auto',
+      placeholder: {
+        '/de-de/': 'Suche',
+        '/zh-cn/': '搜索',
+        '/': 'Search'
+      }
+    }
+  }
+}
